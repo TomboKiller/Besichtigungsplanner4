295338d5885083d7c8c099b97ede57a7d4901a6e
TomboKiller <schini.sch123@gmail.com>
2025-04-08 16:44:47 +0200

    anpassung backend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
-import { NestExpressApplication } from '@nestjs/platform-express'; // Import NestExpressApplication
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule); // Use NestExpressApplication
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,15 +19,6 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-
-  // Set Referrer-Policy header
-  app.use((req, res, next) => {
-    if (process.env.NODE_ENV === 'production') {
-      res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    }
-    next();
-  });
-
   const configService = app.get(ConfigService); // Get ConfigService instance
   const port = configService.get<number>('PORT');
 
